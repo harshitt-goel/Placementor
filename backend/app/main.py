6e1dca67ca7750271e75abf6a57006b3a1cce518
harshitt-goel <hharshit742@gmail.com>
2026-05-16 13:45:08 +0530

    Implemented JWT authentication

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from app.database.db import engine, Base
 from app.models.user_model import User
+from app.routes.auth_routes import router as auth_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
