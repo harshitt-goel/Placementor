83f198add4606a67595b1e76d62d38711e51c080
harshitt-goel <hharshit742@gmail.com>
2026-05-27 14:03:05 +0530

    Fixed Next.js production build

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://interview-platform-hazel.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
